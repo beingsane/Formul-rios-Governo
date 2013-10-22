47bbb1bf130cd4bb4178f45676a680f1bf70328c
Jefersson Nathan <admin@phpse.net>
2013-10-22 20:39:43 -0200

    Update verificacao.php

--- classes/verificacao.php
+++ classes/verificacao.php
@@ -4,15 +4,10 @@
  * @author Jefersson Nathan
  * @date 14/02/13
  * @time 07:52
- * @package Formulário Governo
+ * @package FormulÃ¡rio Governo
  */
 class Verificacao
 {
-    public function __toSting()
-    {
-        echo !1;
-    }
-
     public function verifica($classe)
     {
         if(class_exists($classe))
@@ -23,7 +18,7 @@ class Verificacao
 
 	public static function removerAcento($str){
 		$str = utf8_decode($str);
-		$from = 'ÀÁÃÂÉÊÍÓÕÔÚÜÇàáãâéêíóõôúüç';
+		$from = 'ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½';
 		$to   = 'AAAAEEIOOOUUCaaaaeeiooouuc';
 		return strtr($str, $from, $to);
 	}
@@ -32,39 +27,39 @@ class Verificacao
 
 		$msg  = '';
 		if (empty($dados['nome'])) {
-			$msg .= 'O campo nome não foi preenchido corretamente!'.PHP_EOL;
+			$msg .= 'O campo nome nï¿½o foi preenchido corretamente!'.PHP_EOL;
 
 		}elseif (strlen($dados['nome']) < 3) {
-			$msg .= 'O campo nome deve ter no mínimo 3 caracteres'.PHP_EOL;
+			$msg .= 'O campo nome deve ter no mï¿½nimo 3 caracteres'.PHP_EOL;
 		}
 
 
 		if (!preg_match('#^\d+$#', $dados['identidade'])) {
-			$msg .= 'O campo identidade só suporta números'.PHP_EOL;
+			$msg .= 'O campo identidade sï¿½ suporta nï¿½meros'.PHP_EOL;
 		}
 
 		if (!preg_match('#^\d{3}\.\d{3}\.\d{3}-\d{2}$#', $dados['cpf'])) {
-			$msg .= 'O número do CPF informado não é válido'.PHP_EOL;
+			$msg .= 'O nï¿½mero do CPF informado nï¿½o ï¿½ vï¿½lido'.PHP_EOL;
 		}
 
 		if (!preg_match('#^\d{2}\s\d{4}-\d{4}$#', $dados['telefone'])) {
-			$msg .= 'O número do telefone não é válido - ex: xx xxxx-xxxx'.PHP_EOL;
+			$msg .= 'O nï¿½mero do telefone nï¿½o ï¿½ vï¿½lido - ex: xx xxxx-xxxx'.PHP_EOL;
 		}
 
 		if (!preg_match('#^[A-Za-z0-9._-]+@[A-Za-z]+\.[A-Za-z.]+$#', $dados['e-mail'])) {
-			$msg .= 'O email não é válido'.PHP_EOL;
+			$msg .= 'O email nï¿½o ï¿½ vï¿½lido'.PHP_EOL;
 		}
 
 		if (empty($dados['endereco'])) {
-			$msg .= 'O campo endereço não foi preenchido!'.PHP_EOL;
+			$msg .= 'O campo endereï¿½o nï¿½o foi preenchido!'.PHP_EOL;
 
 			if (strlen($dados['endereco']) < 10) {
-				$msg .= 'Informe melhor o seu endereço'.PHP_EOL;
+				$msg .= 'Informe melhor o seu endereï¿½o'.PHP_EOL;
 			}
 		}
 
 		if ($_FILES['curriculum']['error'] != 0) {
-			$msg .= 'Verifique o arquivo que está sendo enviado!'.PHP_EOL;
+			$msg .= 'Verifique o arquivo que estï¿½ sendo enviado!'.PHP_EOL;
 		} else {
 
 
@@ -80,18 +75,18 @@ class Verificacao
 			if (is_uploaded_file($_FILES['curriculum']['tmp_name']))
 				move_uploaded_file($_FILES['curriculum']['tmp_name'], '/var/www/swapi/istos/sites/ses/form/arquivos_anexados/'.$file_name);
 			else
-				$msg .= 'Verifique o arquivo que está sendo enviado!'.PHP_EOL;
+				$msg .= 'Verifique o arquivo que estï¿½ sendo enviado!'.PHP_EOL;
 
 		}
 
-		/* Referente ao formulário 1 */
+		/* Referente ao formulÃ¡rio 1 */
 		if ( $dados['tipo'] == 'form1') {
 			if (isset($dados['opcao'])) {
 
 				$quantidade = count($dados['opcao']);
 
 				if (! ($quantidade <= 2)) {
-					$msg .= "Você deve escolher entre 1 e 2 opções".PHP_EOL;
+					$msg .= "Vocï¿½ deve escolher entre 1 e 2 opï¿½ï¿½es".PHP_EOL;
 				}
 
 			} else {
@@ -100,7 +95,7 @@ class Verificacao
 
 		}
 
-		/* Referente ao formulário 2 */
+		/* Referente ao formulÃ¡rio 2 */
 		if ( $dados['tipo'] == 'form2') {
 			if (! isset($dados['opcao'])) {
 				$msg .= "Por favor escolha em que cargo deseja inscrever-se ".PHP_EOL;
