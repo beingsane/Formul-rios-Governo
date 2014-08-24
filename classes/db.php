24ce412355ea7e24086f2e63c2f429a8bece8d57
malukenho <admin@phpse.net>
2014-08-24 03:49:57 -0300

    PHPCS Minor fix

--- classes/db.php
+++ classes/db.php
@@ -9,7 +9,7 @@ class DB
 
     public static function getConnection()
     {
-        if(self::$conexao) {
+        if (self::$conexao) {
             self::$conexao =  new PDO('mysql:host=;dbname=','','');
         }
 
